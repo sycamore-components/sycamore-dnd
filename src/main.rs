2af34e426023c9f8ec04e96ffe9243f3eff180eb
Genna Wingert <wingertge@gmail.com>
2023-03-03 10:41:51 +0100

    Clean up example

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use gloo::console::log;
 use serde::{Deserialize, Serialize};
-use sycadrop::{DataTransfer, Draggable, DropEffect, DropTarget};
+use sycadrop::{create_draggable, create_droppable, DropEffect};
 use sycamore::prelude::*;
 
 fn main() {
@@ -23,76 +23,71 @@ struct ContentItem {
 }
 
 #[component]
-fn App<G: Html>(cx: Scope) -> View<G> {
-    let contents = create_rc_signal(vec![
-        ContentItem {
-            id: 0,
-            name: "Test item 0".to_string(),
-        },
-        ContentItem {
-            id: 1,
-            name: "Test item 1".to_string(),
-        },
-        ContentItem {
-            id: 2,
-            name: "Test item 2".to_string(),
-        },
-        ContentItem {
-            id: 3,
-            name: "Test item 3".to_string(),
-        },
-    ]);
-
-    let on_drop = create_ref(cx, {
-        let contents = contents.clone();
-        move |target_id: i32| {
-            let contents = contents.clone();
-            Box::new(move |transfer: DataTransfer| {
-                let dragged_id: i32 = transfer.get_data("text/html").unwrap().parse().unwrap();
-                log!("Swapping {} and {}", target_id, dragged_id);
-                let (dragged_index, target_index) = {
-                    let contents = contents.get();
-                    (
-                        contents.iter().position(|i| i.id == dragged_id).unwrap(),
-                        contents.iter().position(|i| i.id == target_id).unwrap(),
-                    )
-                };
-                let mut contents = contents.modify();
-                contents.swap(dragged_index, target_index);
-            })
-        }
-    });
-
-    let contents = create_ref(cx, contents);
+fn App<'cx, G: Html>(cx: Scope<'cx>) -> View<G> {
+    let contents = create_signal(
+        cx,
+        vec![
+            ContentItem {
+                id: 0,
+                name: "Test item 0".to_string(),
+            },
+            ContentItem {
+                id: 1,
+                name: "Test item 1".to_string(),
+            },
+            ContentItem {
+                id: 2,
+                name: "Test item 2".to_string(),
+            },
+            ContentItem {
+                id: 3,
+                name: "Test item 3".to_string(),
+            },
+        ],
+    );
 
     view! { cx,
         div(class = "container") {
             div(class="box") {
                 Keyed(
                     iterable=contents,
-                    view= move |cx, item| {
-                        let set_data =
-                            move |transfer: DataTransfer| {
-                            transfer.set_data("text/html", &item.id.to_string()).unwrap();
-                    };
-
-                        view! { cx,
-                            Draggable(allowed_effect = DropEffect::Move, set_data = set_data, attr:class = "item", class_dragging = "dragging") {
-                                DropTarget(
-                                    on_drop = on_drop(item.id),
-                                    class_drop_hover = "drag-over",
-                                    attr:style = "width:100%;height:100%",
-                                    attr:data-id = &item.id.to_string()
-                                ) {
-                                    (item.name)
-                                }
-                            }
-                        }
-                    },
-
+                    view= move |cx, item| view! { cx, Item(item = item, items = contents) },
                     key=|item| item.id,
                 )
             }
+        }
+    }
+}
+
+#[component(inline_props)]
+fn Item<'cx, G: Html>(
+    cx: Scope<'cx>,
+    item: ContentItem,
+    items: &'cx Signal<Vec<ContentItem>>,
+) -> View<G> {
+    let node = create_draggable::<G, i32>(cx)
+        .data(item.id)
+        .allowed_effect(DropEffect::Move)
+        .dragging_class("dragging")
+        .build();
+
+    let node = create_droppable::<G, i32>(cx)
+        .node_ref(node)
+        .on_drop({
+            move |dragged_id: i32| {
+                log!("Swapping {} and {}", item.id, dragged_id);
+                let mut items = items.modify();
+                let dragged_index = items.iter().position(|i| i.id == dragged_id).unwrap();
+                let target_index = items.iter().position(|i| i.id == item.id).unwrap();
+                items.swap(dragged_index, target_index);
+            }
+        })
+        .hovering_class("drag-over")
+        .build();
+
+    view! { cx,
+        div(class = "item", data-id = &item.id.to_string(), ref = node) {
+            (item.name)
         }
     }
 }
